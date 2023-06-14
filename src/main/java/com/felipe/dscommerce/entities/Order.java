9cfbd6d09199e7526e572cdec37935037b6e375a
felipeam10 <felipeam10@hotmail.com>
2023-06-14 15:50:33 -0300

    v2.12 OrderItem, OrderItemPK

--- src/main/java/com/felipe/dscommerce/entities/Order.java
+++ src/main/java/com/felipe/dscommerce/entities/Order.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.time.Instant;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -67,6 +68,10 @@ public class Order {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public List<Product> getProducts() {
+        return items.stream().map(x -> x.getProduct()).toList();
     }
 }
 
