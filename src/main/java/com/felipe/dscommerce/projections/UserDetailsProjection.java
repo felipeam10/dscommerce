0e372f29e9a75f391c449b1f37d2a01bc15de374
felipeam10 <felipeam10@hotmail.com>
2023-10-11 11:16:18 -0300

    v5.22 - Implementando o UserDetailsService

---
+++ src/main/java/com/felipe/dscommerce/projections/UserDetailsProjection.java
@@ -0,0 +1,9 @@
+package com.felipe.dscommerce.projections;
+
+public interface UserDetailsProjection {
+
+    String getUsername();
+    String getPassword();
+    Long getRoleId();
+    String getAuthority();
+}
