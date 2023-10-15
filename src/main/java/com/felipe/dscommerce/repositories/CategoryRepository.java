87c0b96a878ee72d94f4b102b073e6b4ab0d8526
felipeam10 <felipeam10@hotmail.com>
2023-10-15 20:05:02 -0300

    v5.32 - Endpoint GET categories

---
+++ src/main/java/com/felipe/dscommerce/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.felipe.dscommerce.repositories;
+
+import com.felipe.dscommerce.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+
+}
