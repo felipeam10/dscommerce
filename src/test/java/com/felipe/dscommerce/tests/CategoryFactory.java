fd19401a1b44757a09005b1f3a46301f7089d937
felipeam10 <felipeam10@hotmail.com>
2023-12-29 01:32:04 -0300

    vJSE-5.18 - Test get categories by id

---
+++ src/test/java/com/felipe/dscommerce/tests/CategoryFactory.java
@@ -0,0 +1,15 @@
+package com.felipe.dscommerce.tests;
+
+import com.felipe.dscommerce.entities.Category;
+
+public class CategoryFactory {
+
+    public static Category createCategory() {
+        return new Category(1L, "Games");
+    }
+
+    public static Category createCategory(Long id, String name) {
+        return new Category(id, name);
+
+    }
+}
