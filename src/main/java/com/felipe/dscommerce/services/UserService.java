1334e4c7e4711263fdfc6ba40dd5e6ff7513ada0
felipeam10 <felipeam10@hotmail.com>
2024-01-09 01:27:03 -0300

    vJSE-5.26 - Improvements to the authenticated() method

--- src/main/java/com/felipe/dscommerce/services/UserService.java
+++ src/main/java/com/felipe/dscommerce/services/UserService.java
@@ -5,6 +5,7 @@ import com.felipe.dscommerce.projections.UserDetailsProjection;
 import com.felipe.dscommerce.entities.Role;
 import com.felipe.dscommerce.entities.User;
 import com.felipe.dscommerce.repositories.UserRepository;
+import com.felipe.dscommerce.util.CustomUserUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -22,6 +23,9 @@ public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private CustomUserUtil customUserUtil;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -41,10 +45,7 @@ public class UserService implements UserDetailsService {
 
     protected User authenticated() {
         try{
-            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-            Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
-            String username = jwtPrincipal.getClaim("username");
-
+            String username = customUserUtil.getLoggedUsername();
             return userRepository.findByEmail(username).get();
         } catch (Exception e) {
             throw new UsernameNotFoundException("User not found");
