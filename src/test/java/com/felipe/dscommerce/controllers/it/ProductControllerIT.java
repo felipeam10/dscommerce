b6d84041eeb22056c8c67028b382b0f52a15d415
felipeam10 <felipeam10@hotmail.com>
2024-02-07 02:05:18 -0300

    vJSE-6.6 - Obtendo token de acesso

--- src/test/java/com/felipe/dscommerce/controllers/it/ProductControllerIT.java
+++ src/test/java/com/felipe/dscommerce/controllers/it/ProductControllerIT.java
@@ -1,5 +1,9 @@
 package com.felipe.dscommerce.controllers.it;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.felipe.dscommerce.dto.ProductDTO;
+import com.felipe.dscommerce.entities.Product;
+import com.felipe.dscommerce.tests.TokenUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +15,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -21,11 +26,28 @@ public class ProductControllerIT {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private TokenUtil tokenUtil;
+    @Autowired
+    private ObjectMapper objectMapper;
+    private String clientToken, adminToken, invalidToken;
+    private String clientUsername, clientPassword, adminUsername, adminPassword;
     private String productName;
+    private ProductDTO productDTO;
+    private Product product;
 
     @BeforeEach
-    public void setUp() {
+    public void setUp() throws Exception {
+        clientUsername = "maria@gmail.com";
+        clientPassword = "123456";
+        adminUsername = "alex@gmail";
+        adminPassword = "123456";
+
         productName = "MacBook";
+
+        clientToken = tokenUtil.obtainAccessToken(mockMvc, clientUsername, clientPassword);
+        adminPassword = tokenUtil.obtainAccessToken(mockMvc, adminUsername, adminPassword);
+        invalidToken = adminToken + "invalidToken"; //simulate an invalid token
     }
 
     @Test
@@ -49,6 +71,17 @@ public class ProductControllerIT {
         result.andExpect(jsonPath("$.content[0].name").value("The Lord of the Rings"));
         result.andExpect(jsonPath("$.content[0].price").value(90.5));
         result.andExpect(jsonPath("$.content[0].imgUrl").value("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg"));
+
+    }
+
+    @Test
+    public void insertShouldReturnProductDTOCreatedWhenAdminLogged() throws Exception {
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+
+        ResultActions result = mockMvc.perform(post("/products")
+                .header("Authorization", "Bearer" + adminToken)
+                .content(jsonBody)
+                .accept(MediaType.APPLICATION_JSON));
 
     }
 }
